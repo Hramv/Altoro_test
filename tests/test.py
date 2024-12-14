b2ac8ff6866e50d66968f480ece254332f468335
Vasiliy Chudakov <vasiliy@example.com>
2024-12-14 12:26:04 +0300

    Added try catch

--- tests/test.py
+++ tests/test.py
@@ -20,6 +20,9 @@ class Parameters(object):
     DEFAULT_USERS_DICT_URL      = "tests/dicts/usernames.txt"
     DEFAULT_PASSWORDS_DICT_URL  = "tests/dicts/passwords.txt"
     TIME_OUT                    = 1
+    ERROR_MESSAGE               = \
+"Во время теста произошла не предвиденная ошибка.\
+ Веб-ресурс Altoromutual скорее всего не доступен."
 
 
 @pytest.fixture(scope="module")
@@ -48,9 +51,13 @@ def test_password_form_masking(page):
     Testing the password form for characters masking
     
     """
+    try:
+        page.open()
+        password_form_type = page.get_password_form().get_attribute("type")
+    except:
+        assert False, Parameters.ERROR_MESSAGE
 
-    page.open()
-    assert page.get_password_form().get_attribute("type") == "password"
+    assert password_form_type == "password"
 
 
 def test_default_creds(page, default_creds_dict):
@@ -62,13 +69,16 @@ def test_default_creds(page, default_creds_dict):
     
     default_creds = []
 
-    for password in default_creds_dict[1]:
-        for username in default_creds_dict[0]:
-            page.open()
-            sig_status = page.sign_in(username, password)
-            if sig_status != page.LOGIN_FAILED_STATUS:
-                default_creds.append([username, password])
-                page.sign_off()
-            sleep(Parameters.TIME_OUT)
+    try:
+        for password in default_creds_dict[1]:
+            for username in default_creds_dict[0]:
+                page.open()
+                sig_status = page.sign_in(username, password)
+                if sig_status != page.LOGIN_FAILED_STATUS:
+                    default_creds.append([username, password])
+                    page.sign_off()
+                sleep(Parameters.TIME_OUT)
+    except:
+        assert False, Parameters.ERROR_MESSAGE
 
     assert not default_creds
