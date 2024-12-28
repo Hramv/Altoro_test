55181aa9dcd98070f1d9feaffd3d2489a4748a9c
Vasiliy Chudakov <vasiliy@example.com>
2024-12-28 15:26:32 +0300

    Fixs

---
+++ framework/elements/TextBox.py
@@ -0,0 +1,16 @@
+from framework.elements.BaseElement import BaseElement
+
+
+class TextBox(BaseElement):
+
+    """
+        A Form Class.
+    """
+
+
+    def __init__(self, locator: tuple, name: str):
+        super().__init__(locator, name)
+
+
+    def send(self, message: str):
+        self.element.send_keys(message)
